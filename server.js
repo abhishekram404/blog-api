c9aa4d9804f7b5a87b002ae9793b2c7e384ed58c
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-12-19 13:19:24 +0545

    Edit origin

--- server.js
+++ server.js
@@ -1,43 +1,56 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 dotenv.config();
-const allowedOrigins = [
-  "https://blog-eight-roan-16.vercel.app/",
-  "https://blog-6dkd0r3wd-abhishekram404.vercel.app/",
-];
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true,
-//     maxAge: "17280000",
-//   })
-// );
-app.options("*", cors());
+// const allowedOrigins = [
+//   "https://blog-eight-roan-16.vercel.app/",
+//   "https://blog-6dkd0r3wd-abhishekram404.vercel.app/",
+// ];
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blog-6dkd0r3wd-abhishekram404.vercel.app/"
+  );
+  next();
+});
+
+app.use(
+  cors({
+    origin: "https://blog-6dkd0r3wd-abhishekram404.vercel.app/",
+    credentials: true,
+    maxAge: "17280000",
+  })
+);
+
+// app.options("*", cors());
+
 // app.use((req, res, next) => {
 //   res.header(
 //     "Access-Control-Allow-Origin",
 //     // ""
 //     "https://blog-6dkd0r3wd-abhishekram404.vercel.app/"
 //   );
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With,content-type"
-  // );
-  next();
-});
+
+//   // res.setHeader("Access-Control-Allow-Credentials", true);
+//   // res.setHeader(
+//   //   "Access-Control-Allow-Methods",
+//   //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   // );
+//   // res.setHeader(
+//   //   "Access-Control-Allow-Headers",
+//   //   "X-Requested-With,content-type"
+//   // );
+//   next();
+// });
+
 app.use(cookieParser());
 app.use(
   bodyParser.json({
@@ -60,13 +73,6 @@ mongoose.connect(
     console.log("✅  Connected to database");
   }
 );
-// app.use(express.static(__dirname + "/app/build/"));
-// if (process.env.NODE_ENV === "production") {
-//   app.get("*", (req, res) => {
-//     res.sendFile(__dirname + "/app/build/index.html");
-//   });
-//   // return;
-// }
 
 app.get("/", (req, res) => res.send("Server is working fine."));
 app.use("/api/user", userRoutes);
