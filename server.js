c9037505316cceaf83c6769d03e52bd6ce298204
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-12-19 13:27:58 +0545

    Allow credentials

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://blog-git-development-abhishekram404.vercel.app"
   );
+  res.header("Access-Control-Allow-Credentials", true);
+
   next();
 });
 
