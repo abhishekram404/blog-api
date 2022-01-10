2a1afb54287f7d9ce8642519e733ab98dbc50fae
Keshav Kishor Ram <55845306+abhishekram404@users.noreply.github.com>
2022-01-10 19:56:21 +0545

    Change Allowed Origin to main

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ dotenv.config();
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://blog-git-development-abhishekram404.vercel.app"
+    // "https://blog-git-development-abhishekram404.vercel.app"
+    "https://blog-git-main-abhishekram404.vercel.app"
   );
   res.header("Access-Control-Allow-Credentials", true);
 
